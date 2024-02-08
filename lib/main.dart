72f23e944208ce261737e96c1a0cd197656c5a10
MhdMuzammilN <muzamil06n@gmail.com>
2024-02-08 20:23:59 +0530

    Whatsapp Tabs with Tabcontroller and Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff075E54),
-          title: const Text("WhatsApp"),
-          titleTextStyle:
-              const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          actions: const [
-            Icon(
-              Icons.camera_alt_outlined,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            Icon(Icons.search, color: Colors.white),
-            SizedBox(
-              width: 15,
-            ),
-            Icon(Icons.more_vert, color: Colors.white),
-            SizedBox(
-              width: 15,
-            ),
-          ],
-        ),
-        body: ChatList(),
-      )),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(child: WhatsAppTabs()));
+  }
+}
+
+class WhatsAppTabs extends StatelessWidget {
+  const WhatsAppTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xff075E54),
+            title: const Text("WhatsApp"),
+            titleTextStyle:
+                const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            actions: const [
+              Icon(
+                Icons.camera_alt_outlined,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Icon(Icons.search, color: Colors.white),
+              SizedBox(
+                width: 15,
+              ),
+              Icon(Icons.more_vert, color: Colors.white),
+              SizedBox(
+                width: 15,
+              ),
+            ],
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.groups_3_outlined,
+                  size: 25,
+                  color: Colors.white,
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Chats",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Updates",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Calls",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ]),
+          ),
+          body: ChatList(),
+        ));
   }
 }
 
